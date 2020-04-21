34ff0ac2ec7dec316d54d2efc2b78a67516119b8
Theraiser2000 <60749786+Theraiser2000@users.noreply.github.com>
2020-04-21 13:58:24 -0700

    Updated with javadoc and updated mechanics

--- app/src/main/java/com/example/project3/Character.java
+++ app/src/main/java/com/example/project3/Character.java
@@ -2,58 +2,130 @@ package com.example.project3;
 
 public class Character {
     private String Name;
-    private int Gold;
     private int Health;
     private int Experience;
+    private int atkStat;
+    private int defStat;
 
-    public  Character(String name){
-        Name = name;
-        Gold = 0;
+    public Character()
+    {
+        Name = "Placeholder";
         Health = 10;
         Experience = 0;
+        atkStat = 1;
+        defStat = 0;
     }
+
+    /**
+     *
+     * @param name is the name
+     */
+    public Character(String name){
+        Name = name;
+        Health = 10;
+        Experience = 0;
+        atkStat = 1;
+        defStat = 0;
+    }
+
+    /**
+     *
+     * @return the defense stat
+     */
+    int getAtkStat()
+    {
+        return atkStat;
+    }
+
+    /**
+     *
+     * @return the defense stat
+     */
+    int getDefStat()
+    {
+        return defStat;
+    }
+
+    /**
+     *
+     * @param stat is the stat to set for offense
+     */
+    void setAtkStat(int stat)
+    {
+        atkStat = stat;
+    }
+
+    /**
+     *
+     * @param stat is the stat to set for defense
+     */
+    void setDefStat(int stat)
+    {
+        defStat = stat;
+    }
+
+    /**
+     *
+     * @param name is the name
+     */
     void setName(String name){
         Name = name;
     }
+
+    /**
+     *
+     * @return the name
+     */
     String getName(){
         return Name;
     }
-    void setGold(int gold){
-        Gold = gold;
-    }
-    int getGold(){
-        return Gold;
-    }
+
+    /**
+     *
+     * @param health is the amount of health
+     */
     void setHealth(int health){
         Health = health;
     }
+
+    /**
+     *
+     * @return the health
+     */
     int getHealth(){
         return Health;
     }
-    void setExperience(int xp){
-        Experience = xp;
-    }
 
+    /**
+     *
+     * @param damage is the damage taken
+     */
     void takeDamage(int damage){
         Health -= damage;
     }
+
+    /**
+     *
+     * @param health is the health healed
+     */
     void heal(int health){
         Health += health;
     }
 
-    void gainGold(int gold){
-        Gold += gold;
-    }
-    boolean payGold(int total){
-        if(total < Gold){
-            Gold -= total;
-            return true;
-        }
-        else
-            return false;
-    }
-
+    /**
+     *
+     * @param xp is the experience points gained
+     */
     void gainXP(int xp){
         Experience += xp;
+    }
+
+    /**
+     *
+     * @return the amount of experience
+     */
+    int getExperience()
+    {
+        return Experience;
     }
 }
